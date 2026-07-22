7a8774adcbc7fca17abc04a0d8ef0f11540e5e33
CHIRAG-TN <tnchirag@gmail.com>
2026-07-23 01:24:34 +0530

    basic Auth Security using InMemoryUserDetailsManager

--- src/main/java/com/example/demo/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,8 +25,40 @@ public class SecurityConfig {
 		http.csrf(customizer -> customizer.disable())
 			.authorizeHttpRequests(request -> request.anyRequest().authenticated())
 			.httpBasic(Customizer.withDefaults())
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
 		return http.build();
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+		
+		UserDetails user1= User.withUsername("user1")
+									 .password(passwordEncoder.encode("123456"))
+									 .build();
+		
+		UserDetails user2= User.withUsername("user2")
+				 .password(passwordEncoder.encode("1234567"))
+				 .build();
+		
+		return new InMemoryUserDetailsManager(user1,user2);
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
